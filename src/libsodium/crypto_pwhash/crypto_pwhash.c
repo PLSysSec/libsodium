3f684d9de38de98742f3c76104d8aafb57f07c9f
Ahuang <zoe.ahuang@gmail.com>
2017-10-15 21:22:36 -0700

    checked crypto_pwhash.c

--- src/libsodium/crypto_pwhash/crypto_pwhash.c
+++ src/libsodium/crypto_pwhash/crypto_pwhash.c
@@ -5,126 +5,149 @@
 #include "core.h"
 #include "crypto_pwhash.h"
 
+// not interesting
 int
 crypto_pwhash_alg_argon2i13(void)
 {
     return crypto_pwhash_ALG_ARGON2I13;
 }
 
+// not interesting
 int
 crypto_pwhash_alg_argon2id13(void)
 {
     return crypto_pwhash_ALG_ARGON2ID13;
 }
 
+// not interesting
 int
 crypto_pwhash_alg_default(void)
 {
     return crypto_pwhash_ALG_DEFAULT;
 }
 
+// not interesting
 size_t
 crypto_pwhash_bytes_min(void)
 {
     return crypto_pwhash_BYTES_MIN;
 }
 
+// not interesting
 size_t
 crypto_pwhash_bytes_max(void)
 {
     return crypto_pwhash_BYTES_MAX;
 }
 
+// not interesting
 size_t
 crypto_pwhash_passwd_min(void)
 {
     return crypto_pwhash_PASSWD_MIN;
 }
 
+// not interesting
 size_t
 crypto_pwhash_passwd_max(void)
 {
     return crypto_pwhash_PASSWD_MAX;
 }
 
+// not interesting
 size_t
 crypto_pwhash_saltbytes(void)
 {
     return crypto_pwhash_SALTBYTES;
 }
 
+// not interesting
 size_t
 crypto_pwhash_strbytes(void)
 {
     return crypto_pwhash_STRBYTES;
 }
 
+// not intersting
 const char *
 crypto_pwhash_strprefix(void)
 {
     return crypto_pwhash_STRPREFIX;
 }
 
+// not interesting
 size_t
 crypto_pwhash_opslimit_min(void)
 {
     return crypto_pwhash_OPSLIMIT_MIN;
 }
 
+// not interesting
 size_t
 crypto_pwhash_opslimit_max(void)
 {
     return crypto_pwhash_OPSLIMIT_MAX;
 }
 
+// not interesting
 size_t
 crypto_pwhash_memlimit_min(void)
 {
     return crypto_pwhash_MEMLIMIT_MIN;
 }
 
+// not interesting
 size_t
 crypto_pwhash_memlimit_max(void)
 {
     return crypto_pwhash_MEMLIMIT_MAX;
 }
 
+// not interesting
 size_t
 crypto_pwhash_opslimit_interactive(void)
 {
     return crypto_pwhash_OPSLIMIT_INTERACTIVE;
 }
 
+// not interesting
 size_t
 crypto_pwhash_memlimit_interactive(void)
 {
     return crypto_pwhash_MEMLIMIT_INTERACTIVE;
 }
 
+// not interesting
 size_t
 crypto_pwhash_opslimit_moderate(void)
 {
     return crypto_pwhash_OPSLIMIT_MODERATE;
 }
 
+// not interesting
 size_t
 crypto_pwhash_memlimit_moderate(void)
 {
     return crypto_pwhash_MEMLIMIT_MODERATE;
 }
 
+// not interesting
 size_t
 crypto_pwhash_opslimit_sensitive(void)
 {
     return crypto_pwhash_OPSLIMIT_SENSITIVE;
 }
 
+// not interesting
 size_t
 crypto_pwhash_memlimit_sensitive(void)
 {
     return crypto_pwhash_MEMLIMIT_SENSITIVE;
 }
 
+// FACT
+// used switch statement to call functions,
+// we can use if else to achieve the same effect
 int
 crypto_pwhash(unsigned char * const out, unsigned long long outlen,
               const char * const passwd, unsigned long long passwdlen,
@@ -144,6 +167,7 @@ crypto_pwhash(unsigned char * const out, unsigned long long outlen,
     }
 }
 
+// wrapper function
 int
 crypto_pwhash_str(char out[crypto_pwhash_STRBYTES],
                   const char * const passwd, unsigned long long passwdlen,
@@ -153,6 +177,8 @@ crypto_pwhash_str(char out[crypto_pwhash_STRBYTES],
                                       opslimit, memlimit);
 }
 
+// FACT
+// used switch statement
 int
 crypto_pwhash_str_alg(char out[crypto_pwhash_STRBYTES],
                       const char * const passwd, unsigned long long passwdlen,
@@ -170,6 +196,8 @@ crypto_pwhash_str_alg(char out[crypto_pwhash_STRBYTES],
     /* NOTREACHED */
 }
 
+// FACT
+// used strncmp in the if condition 
 int
 crypto_pwhash_str_verify(const char str[crypto_pwhash_STRBYTES],
                          const char * const passwd,
@@ -188,6 +216,8 @@ crypto_pwhash_str_verify(const char str[crypto_pwhash_STRBYTES],
     return -1;
 }
 
+// FACT
+// same as the previouse function, used strncmp in the if condition
 int
 crypto_pwhash_str_needs_rehash(const char str[crypto_pwhash_STRBYTES],
                                unsigned long long opslimit, size_t memlimit)
@@ -205,6 +235,7 @@ crypto_pwhash_str_needs_rehash(const char str[crypto_pwhash_STRBYTES],
     return -1;
 }
 
+// not interesting
 const char *
 crypto_pwhash_primitive(void) {
     return crypto_pwhash_PRIMITIVE;
